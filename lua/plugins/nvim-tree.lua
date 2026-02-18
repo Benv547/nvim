59566720f1004a4969ad0a2325667eb2d6d0c3ba
Benjamin Vignot <benjamin.vignot@lacos.de>
2026-02-18 17:38:26 +0100

    nvim tree: open external command with $

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -6,8 +6,30 @@ return {
     "nvim-tree/nvim-web-devicons",
   },
   config = function()
-    require("nvim-tree").setup({})
+    require("nvim-tree").setup({
+      on_attach = function(bufnr)
+        local api = require("nvim-tree.api")
 
+        -- Mappings par défaut
+        api.config.mappings.default_on_attach(bufnr)
+
+        -- Mapping $ pour exécuter une commande dans le chemin courant
+        vim.keymap.set("n", "$", function()
+          local node = api.tree.get_node_under_cursor()
+          local path
+
+          -- Si c'est un fichier, prendre le dossier parent
+          if node.type == "file" then
+            path = vim.fn.fnamemodify(node.absolute_path, ":h")
+          else
+            path = node.absolute_path
+          end
+
+          -- Ouvrir le command-line avec la commande préparée
+          vim.fn.feedkeys(":! cd " .. vim.fn.shellescape(path) .. " && ", "n")
+        end, { buffer = bufnr, desc = "nvim-tree: Run command in current path" })
+      end,
+    })
     -- Wrapper toggle : réapplique le layout de la sidebar après ouverture/fermeture
     vim.keymap.set("n", "<leader>e", function()
       vim.cmd("NvimTreeFindFileToggle")
@@ -16,3 +38,4 @@ return {
     end, { desc = "Ouverture/fermeture de l'explorateur de fichiers" })
   end,
 }
+
